4f495b765c619755e34db2adae6d8ad8f3c9ec66
Ferreira Assis Gomes <ferreirassisg@gmail.com>
2024-02-12 18:05:58 +0100

    ensure HttpAdapter throws BadRequestError on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -5,7 +5,6 @@ import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-
 import 'package:fordev/data/http/http.dart';
 
 import 'package:fordev/infra/http/http.dart';
@@ -85,11 +84,20 @@ void main() {
     });
 
     test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400, body: '');
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
       mockResponse(400);
 
       final future = sut.request(url: url, method: 'post');
 
       expect(future, throwsA(HttpError.badRequest));
     });
+
   });
 }
