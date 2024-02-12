7e94f0bc3a0591c54266dd43f8efa759a82a5656
Ferreira Assis Gomes <ferreirassisg@gmail.com>
2024-02-12 20:46:32 +0100

    change statusBar to light mode

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -5,13 +5,14 @@ import '../components/component.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             LoginHeader(),
-            HeadLine1(text:"Login"),
+            HeadLine1(text: "Login"),
             Padding(
               padding: EdgeInsets.all(32),
               child: Form(
@@ -19,14 +20,22 @@ class LoginPage extends StatelessWidget {
                 children: <Widget>[
                   TextFormField(
                     decoration: InputDecoration(
-                        labelText: 'Email', icon: Icon(Icons.email, color: Theme.of(context).primaryColorLight,)),
+                        labelText: 'Email',
+                        icon: Icon(
+                          Icons.email,
+                          color: Theme.of(context).primaryColorLight,
+                        )),
                     keyboardType: TextInputType.emailAddress,
                   ),
                   Padding(
                     padding: const EdgeInsets.only(top: 8, bottom: 32),
                     child: TextFormField(
                       decoration: InputDecoration(
-                          labelText: 'Senha', icon: Icon(Icons.lock,  color: Theme.of(context).primaryColorLight,)),
+                          labelText: 'Senha',
+                          icon: Icon(
+                            Icons.lock,
+                            color: Theme.of(context).primaryColorLight,
+                          )),
                       obscureText: true,
                     ),
                   ),
