893ab60bcdb5472e5053b01e64b2478792a095d4
Ferreira Assis Gomes <ferreirassisg@gmail.com>
2024-02-12 17:30:31 +0100

    ensure HttpAdapter returns data if post returns 204 with data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -19,7 +19,11 @@ class HttpAdapter implements HttpClient {
     final jsonBody = body != null ? jsonEncode(body) : null;
     final response =
         await client.post(Uri.parse(url), headers: headers, body: jsonBody);
-    return response.body.isEmpty ? null : jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else {
+      return {};
+    }
   }
 }
 
@@ -81,8 +85,16 @@ void main() {
       expect(response, null);
     });
 
-     test('Should return null if post returns 204 with no data', () async {
+    test('Should return null if post returns 204', () async {
       mockResponse(204, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+
+    test('Should return null if post returns 204 with data', () async {
+      mockResponse(204);
 
       final response = await sut.request(url: url, method: 'post');
 
