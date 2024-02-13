c7c43fadab4f9df60762cd2552ed1dea182b073b
Ferreira Assis Gomes <ferreirassisg@gmail.com>
2024-02-13 02:02:48 +0100

    ensure loginPage hide loading

--- test/ui/pages/login_page.dart
+++ test/ui/pages/login_page.dart
@@ -53,6 +53,7 @@ void main() {
     final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
 
     expect(button.onPressed, null);
+    expect(find.byType(CircularProgressIndicator), findsNothing);
   });
 
   testWidgets('should call validate with correct values',
@@ -143,5 +144,15 @@ void main() {
     await tester.pump();
 
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('should hide loading', (WidgetTester tester) async {
+    await loadPage(tester);
+    isLoadingController.add(true);
+    await tester.pump();
+    isLoadingController.add(false);
+    await tester.pump();
+
+    expect(find.byType(CircularProgressIndicator), findsNothing);
   });
 }
