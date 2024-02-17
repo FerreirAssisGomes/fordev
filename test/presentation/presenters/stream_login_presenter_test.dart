0d0a343f88ae95ab74b73b946efc2330eaf9ad7d
Ferreira Assis Gomes <ferreirassisg@gmail.com>
2024-02-17 17:44:47 +0100

    move validation to its own file

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,30 +1,30 @@
 import 'dart:async';
 
 import 'package:faker/faker.dart';
+import 'package:fordev/presentation/protocols/validation.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-abstract class Validation {
-  String? validate({required String field, required String value});
-}
+
 
 class LoginState {
-  late String emailError;
+  String emailError;
+
+  LoginState(this.emailError);
 }
 
 class StreamLoginPresenter {
   final Validation validation;
   final _controller = StreamController<LoginState>.broadcast();
+  final _state = LoginState("error");
 
-  final _state = LoginState();
+  StreamLoginPresenter({required this.validation});
 
   Stream<String> get emailErrorStream =>
       _controller.stream.map((state) => state.emailError);
 
-  StreamLoginPresenter({required this.validation});
-
   void validateEmail(String email) {
-    _state.emailError = validation.validate(field: 'email', value: email)!;
+    _state.emailError = validation.validate(field: 'email', value: email)??"";
     _controller.add(_state);
   }
 }
