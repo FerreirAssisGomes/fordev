da0bd5f7f42baf8a1e86fc91263e22fc6f228e18
Ferreira Assis Gomes <ferreirassisg@gmail.com>
2024-02-17 17:50:05 +0100

    move StreamLoginPresenter and to its own file

---
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -0,0 +1,26 @@
+
+import 'dart:async';
+
+import '../protocols/protocols.dart';
+
+class LoginState {
+  String emailError;
+
+  LoginState(this.emailError);
+}
+
+class StreamLoginPresenter {
+  final Validation validation;
+  final _controller = StreamController<LoginState>.broadcast();
+  final _state = LoginState("error");
+
+  StreamLoginPresenter({required this.validation});
+
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
+
+  void validateEmail(String email) {
+    _state.emailError = validation.validate(field: 'email', value: email)??"";
+    _controller.add(_state);
+  }
+}
