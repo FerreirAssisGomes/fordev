9c127fa0562179b7b072b6dba0efcbfccd825c61
Ferreira Assis Gomes <ferreirassisg@gmail.com>
2024-02-27 09:16:35 +0100

    add debugCheckInvalidValueType

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/route_manager.dart';
+import 'package:provider/provider.dart';
 
 import '../ui/components/component.dart';
 import './factories/factories.dart';
 
 void main() {
+  Provider.debugCheckInvalidValueType = null;
   runApp(App());
 }
 
