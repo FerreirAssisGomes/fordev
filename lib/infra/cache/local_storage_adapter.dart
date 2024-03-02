eb04bbe4c9ca27692a3797798e397783828a4919
Ferreira Assis Gomes <ferreirassisg@gmail.com>
2024-03-02 23:32:18 +0100

    move LocalStorageAdapter to its own file

---
+++ lib/infra/cache/local_storage_adapter.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import '../../data/cache/cache.dart';
+
+class LocalStorageAdapter implements SaveSecureCacheStorage {
+  final FlutterSecureStorage secureStorage;
+  LocalStorageAdapter(this.secureStorage);
+  Future<void> saveSecure({required String key, required String value}) async {
+    await secureStorage.write(key: key, value: value);
+  }
+}
--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -1,16 +1,9 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:fordev/infra/cache/cache.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:fordev/data/cache/cache.dart';
 
-class LocalStorageAdapter implements SaveSecureCacheStorage {
-  final FlutterSecureStorage secureStorage;
-  LocalStorageAdapter(this.secureStorage);
-  Future<void> saveSecure({required String key, required String value}) async {
-    await secureStorage.write(key: key, value: value);
-  }
-}
 
 class FlutterSecureStorageMockSpy extends Mock implements FlutterSecureStorage {
 }
